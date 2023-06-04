9ea0c43652d0e3b2bc0ffc1d747fa08657fee9e9
yeemans <arthur.roth.07@gmail.com>
2023-06-03 21:19:39 -0700

    added functionality to recommend songs from a different genre

--- recommender.py
+++ recommender.py
@@ -11,6 +11,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import os
 import sys
 import requests
+from collections import Counter
+import random
 
 clientId= sys.argv[1]
 clientSecret = sys.argv[2]
@@ -53,7 +55,6 @@ def encodeGenreAndKey(spotify_features_df):
 
 #creating the playlist dataframe with extended features using Spotify data
 def generate_playlist_df(playlist, spotify_data):
-    
     playlist_df = pd.DataFrame()
 
     for i, j in enumerate(playlist['tracks']['items']):
@@ -69,7 +70,7 @@ def generate_playlist_df(playlist, spotify_data):
 
     return playlist_df 
 
-def generate_playlist_vector(spotify_features, playlist_df, weight_factor):
+def generate_playlist_vector(spotify_features, playlist_df):
     spotify_features_playlist = spotify_features[spotify_features['track_id'].isin(playlist_df['track_id'].values)]
     spotify_features_playlist = spotify_features_playlist.merge(playlist_df[['track_id','date_added']], on = 'track_id', how = 'inner')
     
@@ -77,39 +78,71 @@ def generate_playlist_vector(spotify_features, playlist_df, weight_factor):
     return spotify_features_playlist.sum(axis = 0), spotify_features_nonplaylist
 
 def generate_recommendation(spotify_data, playlist_vector, nonplaylist_df):
-
     non_playlist = spotify_data[spotify_data['track_id'].isin(nonplaylist_df['track_id'].values)]
-    print([spotify_data.shape, non_playlist.shape])
     non_playlist['sim'] = cosine_similarity(nonplaylist_df.drop(['track_id'], axis = 1).values, playlist_vector.drop(labels = 'track_id').values.reshape(1, -1))[:,0]
     non_playlist = non_playlist.sort_values('sim',ascending = False).iloc[:15]
     
-    print("non")
-    print(non_playlist)
-    return  non_playlist#_top15
+    return non_playlist
+
+def get_new_genre(playlist): 
+    genreCounts = Counter()
+    songCount = 0 
+
+    for key in playlist.keys(): 
+        if key not in genres: continue
+        genreCounts[key] += playlist[key]
+        songCount += playlist[key]
+    
+    newGenres = set(genres.copy()) # genres in this set make up less than 5% of the playlist 
+    for key in playlist.keys(): 
+        if genreCounts[key] / songCount >= 0.05: newGenres -= {key}
+
+    # get a random genre from newGenres
+    randomGenre = random.choice(tuple(newGenres))
+    return randomGenre
+
+
+
+def generate_other_genre_recommendation(spotify_data, playlist_vector, nonplaylist_df): 
+    non_playlist = spotify_data[spotify_data['track_id'].isin(nonplaylist_df['track_id'].values)]
+
+    randomGenre = get_new_genre(playlist_vector)
+    print("random genre: " + randomGenre)
+    non_playlist = non_playlist[non_playlist["genre"] == randomGenre]
+    nonplaylist_df = nonplaylist_df[nonplaylist_df[randomGenre] == 1] # because of one-hot encoding
+
+    print("non_playlist_df:")
+    print([non_playlist.shape, nonplaylist_df.shape])
+
+    non_playlist['sim'] = cosine_similarity(nonplaylist_df.drop(['track_id'], axis = 1).values, playlist_vector.drop(labels = 'track_id').values.reshape(1, -1))[:,0]
+    non_playlist = non_playlist.sort_values('sim',ascending = False).iloc[:15]
+    
+    return non_playlist
+   
 
 data = pd.read_csv("SpotifyFeatures.csv")
 spotify_features_df = data
+genres = spotify_features_df.genre.unique()
 spotify_features_df = normalize(spotify_features_df)
 spotify_features_df = encodeGenreAndKey(spotify_features_df)
 spotify_features_df = dropColumns(spotify_features_df)
 
-access_token = "BQDjtWsioOkcg-dKL9zxhrAVP1OHkTCwZLPlRQo-D02JVdeWIEZi2qeY3sYspSJBr3mI8M7ySJWV7r-_UcvJuc0P966m8CX9w_ESlAFgdZ_1AR8i6t8"
+access_token = "BQCJg2u_JOaJeKmvSElRmZy4PLiJ6un2AJ1az9Ukah--amidFNSmpQT9LayYNE6u-Tgud_K44CRx-1ueqZh53j0Td11nkVcSwecG6fzaPPbQUBQmjS0"
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
-# base URL of all Spotify API endpoints
 BASE_URL = 'https://api.spotify.com/v1/'
 
 # actual GET request with proper header
-r = requests.get(BASE_URL + 'playlists/37i9dQZF1DX0kbJZpiYdZl', headers=headers)
+r = requests.get(BASE_URL + 'playlists/37i9dQZF1DXcBWIGoYBM5M', headers=headers)
 json = r.json()
 playlist_df = generate_playlist_df(json, data)
-print(data.columns)
-playlist_vector, nonplaylist_df = generate_playlist_vector(spotify_features_df, playlist_df, 1.2)
-print(playlist_vector.shape)
-print(nonplaylist_df.head())
+playlist_vector, nonplaylist_df = generate_playlist_vector(spotify_features_df, playlist_df)
 
-# recs 
-print([playlist_vector.shape, nonplaylist_df.shape])
+# recommend
 top15 = generate_recommendation(data, playlist_vector, nonplaylist_df)  
-top15.head()
+print(top15.head())
+
+# recommend songs that are similar but have a different genre
+top15 = generate_other_genre_recommendation(data, playlist_vector, nonplaylist_df)
+print(top15)
